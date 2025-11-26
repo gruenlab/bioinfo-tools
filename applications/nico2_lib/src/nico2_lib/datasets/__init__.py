24dd4fc238f15368a3e0ab7ae4534fa4b8afec57
egerc <153100377+egerc@users.noreply.github.com>
2025-11-26 10:58:49 +0100

    .

--- applications/nico2_lib/src/nico2_lib/datasets/__init__.py
+++ applications/nico2_lib/src/nico2_lib/datasets/__init__.py
@@ -1,3 +1,5 @@
 from nico2_lib.datasets._xenium_10x import xenium_10x_loader
+from nico2_lib.datasets._cell_atlases import human_liver_cell_atlas
 
-__all__ = ["xenium_10x_loader"]
+
+__all__ = ["xenium_10x_loader", "human_liver_cell_atlas"]
