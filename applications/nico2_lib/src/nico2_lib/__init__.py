2cfdd76600ce136909004f05912672640ff7ad0c
egerc <eger.christian@gmx.de>
2026-01-09 12:02:17 +0100

    shorthands for the modules added

---
+++ applications/nico2_lib/src/nico2_lib/__init__.py
@@ -0,0 +1,6 @@
+from nico2_lib import datasets as dt
+from nico2_lib import label_transfer as lt
+from nico2_lib import metrics as mt
+from nico2_lib import predictors as pd
+
+__all__ = ["dt", "mt", "pd", "lt"]
