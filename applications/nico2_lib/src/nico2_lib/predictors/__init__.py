bc89d5004951af2dd4538da77f9758afd4feb5ec
egerc <153100377+egerc@users.noreply.github.com>
2026-04-29 13:36:59 +0200

    added missing import for scvi predictor

--- applications/nico2_lib/src/nico2_lib/predictors/__init__.py
+++ applications/nico2_lib/src/nico2_lib/predictors/__init__.py
@@ -2,6 +2,7 @@ from nico2_lib.predictors._baselines import shuffle_by_embedding_neighbors
 from nico2_lib.predictors._mofaflex._mofaflex_pred import MofaFlexPredictor
 from nico2_lib.predictors._nmf._nmf_pred import NmfPredictor
 from nico2_lib.predictors._protocol import PredictorProtocol
+from nico2_lib.predictors._scvi._scvi_pred import ScviPredictor
 from nico2_lib.predictors._tangram._tangram_pred import TangramPredictor
 
 __all__ = [
@@ -10,4 +11,5 @@ __all__ = [
     "PredictorProtocol",
     "shuffle_by_embedding_neighbors",
     "MofaFlexPredictor",
+    "ScviPredictor",
 ]
